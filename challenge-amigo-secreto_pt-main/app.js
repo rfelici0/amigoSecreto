4189e08ccb31d94a5d3de873a064ac4e2993e872
rafael felicio <rafafelicio2311@gmail.com>
2025-08-27 10:47:42 -0300

    finalização do códgo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,17 +1,38 @@
-let  amigos = [];
+let  amigos = []; // Array para armazenar os nomes dos amigos
 
+// função para adicionar um amigo
 function adicionarAmigo() {
-    let amigo = document.querySelector('input').value;
-        amigos.push(amigo);
-        console.log(amigos);
-    
-    if (amigo != ''){
-        atualizarLista();  
+    let nome = document.querySelector('input').value.trim();// Seleciona o valor do input e remove espaços em branco
+
+    if (nome !== ''){// Verifica se o nome não está vazio
+        amigos.push(nome);// Adiciona o nome ao array de amigos
+        atualizarLista();
     }else{
-        alert('digite um nome valido');
+        alert('Digite um nome válido');
     }    
 }
 
+// função para atualizar a lista de amigos
 function atualizarLista(){
-    let lista = document.querySelector('#listaAmigos');
+    let lista = document.querySelector('#listaAmigos');// Seleciona a lista de amigos
+    lista.innerHTML = ''; // Limpa a lista atual
+    amigos.forEach((amigo, index) => {
+        let li = document.createElement('li');// Cria um novo item de lista
+        li.textContent = amigo; // Define o texto do item como o nome do amigo
+        lista.appendChild(li); // Adiciona o item à lista
+    });
 }
+
+// função para sortear os amigos
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Adicione pelo menos dois amigos para sortear.');
+        return;
+    }
+    let sorteado = amigos [Math.floor(Math.random() * amigos.length)]; // Seleciona um amigo aleatoriamente
+    let resultado = document.querySelector("#resultado");
+    resultado.innerHTML = ""; // limpa resultado anterior
+    let li = document.createElement('li');
+    li.textContent = `O amigo sorteado foi: ${sorteado}`; // Define o texto do item como o nome do amigo sorteado
+    resultado.appendChild(li); // Adiciona o item ao resultado
+}    
